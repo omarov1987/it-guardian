b64559ffa88ceb7d8451d9bc98729006c7fc365e
Omar <omar.hlehel@gmail.com>
2026-04-21 09:52:53 +0200

    add owner_id and user model

--- models.py
+++ models.py
@@ -2,10 +2,15 @@ from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from datetime import datetime
 from database import Base
 
+
 class Device(Base):
     __tablename__ = "devices"
 
     hostname = Column(String, primary_key=True, index=True)
+
+   
+   # owner_id = Column(Integer)
+
     os = Column(String)
     disk_free = Column(Integer)
     last_seen = Column(DateTime, default=datetime.utcnow)
@@ -13,3 +18,11 @@ class Device(Base):
     # alert flags
     offline_alert_sent = Column(Boolean, default=False)
     disk_alert_sent = Column(Boolean, default=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
